6b6a6d577aed4e167f96061ca1d8a3b1a7bf4221
Erick <reemcata8141@gmail.com>
2024-03-18 22:34:06 -0700

    Added damage to enemy, Win/Lose and Hit timer

--- sketch.js
+++ sketch.js
@@ -4,10 +4,9 @@ let growBomb = [];
 let bullets = [];
 let cloud1Obj = [];
 let cloud2Obj = [];
-let numClouds = 5;
-let mySound;
-let PlayerX = 0;
-let PlayerY = 0;
+let numClouds = 10;
+let PlayerX = 800;
+let PlayerY = 400;
 let delay = 0.03;
 let enemy;
 let player;
@@ -16,28 +15,40 @@ let Health = 0;
 let maxSpeed = 8;
 let enemyY = 100;
 let mysound;
-
+let GameOver = false; //GAME OVER
+let restartDeath = 0;
+let HitCooldown = false;
+let HitTimer = 0;
 function preload(){
   enemy = loadImage('assets/Enemy.png');
   player = loadImage('assets/PlayerJet.png');
   boom = loadImage('assets/Explode.png');
   cloud1 = loadImage('assets/Cloud1.png');
   cloud2 = loadImage('assets/Cloud2.png');
+  enemyBG = loadImage('assets/EnemyBG.png');
   gif = createImg('assets/Ground1.gif');
   gif.hide();
   soundFormats('mp3', 'ogg','wav');
-  mySound = loadSound('assets/bonk.wav');
+  PlayerShoot = loadSound('assets/PlayerShoot.wav');
+  PlayerDead = loadSound('assets/PlayerDead.wav');
+  EnemyHit = loadSound('assets/EnemyHit.wav');
+  EnemyShoot = loadSound('assets/EnemyShoot.wav');
+  Win = loadSound('assets/Victory.wav');
 }
 
 function setup() {
   createCanvas(1200,900);
-  
+  enemyBGx = random(50,1150);
+  enemyBGy = -10;
+  enemyBGsize = random(10,35);
+  GameOver = false;
   time = 0;
   dropper = 15;
   dropperSpeed = 5;
   dropperFlip = false
   loop = 0;
   bombs = 100;
+
   for (let i = 0; i < numBombs; i++){ //Red Bomb
     Obombs[i] = {
       x: dropper,
@@ -46,9 +57,9 @@ function setup() {
       bombSpeed: random(3,maxSpeed)
     }
   }
-BombSize = 10;
-growRate = 2; //Grow Bomb Rate
-growBomb = {
+  BombSize = 10;
+  growRate = 2; //Grow Bomb Rate
+  growBomb = {
     x: dropper,
     y: 100,
     diameter: BombSize,
@@ -58,23 +69,32 @@ growBomb = {
   cloud1Obj[i] = {
     x: random(width), 
     y: random(-200, -100), 
-    speed: random(1, 3),
-    size: random(100,250)
-  };
-}
-for (let i = 0; i < numClouds; i++){
+    speed: random(.5, 3),
+    size: random(150,350)
+    };
+  }
+  for (let i = 0; i < numClouds; i++){
   cloud2Obj[i] = {
     x: random(width), 
     y: random(-200, -100), 
-    speed: random(1, 3),
-    size: random(100,250) 
+    speed: random(.5, 3),
+    size: random(100,450) 
     };
   }
 }
-
+function restartGame(){ // R E S T A R T ===================
+  location.reload();
+}
 function draw(){
   background(0,50,180);
   image(gif, width/2, height/2, 1200, 900);
+  image(enemyBG,enemyBGx,enemyBGy,enemyBGsize,enemyBGsize);//Background enemy
+  enemyBGy += 2;
+  if (enemyBGy > 920){
+    enemyBGy = -20;
+    enemyBGx = random(50,1150);
+    enemyBGsize = random(10,35);
+  }
   textSize(20);
   fill(0);
   text("Time: " + time, 20, 40);
@@ -125,11 +145,13 @@ function draw(){
     }
     strokeWeight(2)
     ellipse(bombObj.x,bombObj.y,bombObj.diameter,bombObj.diameter);
+    
 
   if (bombObj.y > height + 50){
       bombObj.y = 100;
       bombObj.x = dropper;
       bombObj.bombSpeed = random(3,8);
+      EnemyShoot.play();
     }
   }
 
@@ -148,18 +170,67 @@ function draw(){
     growBomb.diameter = random(10, 20); 
     growBomb.bombSpeed = random(3, maxSpeed); 
   }
-strokeWeight(0)
+  for (let i = 0; i < Obombs.length; i++) { //Player Dead BOMBS
+    let bombObj = Obombs[i];
+    if (dist(PlayerX, PlayerY, bombObj.x, bombObj.y) < bombObj.diameter / 2 + 20) {
+      console.log("Player hit by bomb!");
+      image(boom,PlayerX,PlayerY,100,100);
+      PlayerDead.play();
+      GameOver = true;
+      textSize(50);
+      stroke(255,180,180);
+      fill(200,40,40);
+      text("Click 3 Times to Restart ",width/4, height/2);
+      noLoop();
+    }
+  }
+ //Player Dead GROW BOMB
+  if (dist(PlayerX, PlayerY, growBomb.x, growBomb.y) < growBomb.diameter / 2 + 10) {
+      console.log("Player hit by bomb!");
+      image(boom,PlayerX,PlayerY,100,100);
+      PlayerDead.play();
+      GameOver = true;
+      textSize(50);
+      stroke(255,180,180);
+      fill(200,40,40);
+      text("Click 3 Times to Restart ",width/4, height/2);
+      noLoop();
+    }
+  
+  strokeWeight(0)
   for (let i = bullets.length - 1; i >= 0; i--) { //bullet
     let bullet = bullets[i];
     bullet.y += bullet.bulletSpeed;
     fill(255,255,100);
-    
-    ellipse(bullet.x, bullet.y, 5, 10);
-    if (dist(bullet.x, bullet.y, dropper, 100) < 40) { //Enemy get hit
+
+    ellipse(bullet.x, bullet.y, 5, 10); //ENEMY HIT
+    if (HitCooldown === false && dist(bullet.x, bullet.y, dropper, 100) < 40) { //Enemy get hit
       console.log("Enemy hit!");
-      Health ++;
-      image(boom,dropper,enemyY,70,70);
+      HitCooldown = true;
+      Health += 4;
+      
+      EnemyHit.play();
    }
+   if (HitCooldown === true){
+      HitTimer ++;
+      image(boom,dropper,enemyY,70,70);
+      if (HitTimer > 30){
+        HitCooldown = false;
+        HitTimer = 0;
+      }
+   }
+   if (Health > 500){   // G A M E  W I N ==========
+    GameOver = true
+    textSize(75);
+    fill(30,255,80);
+    stroke(255,255,0);
+    strokeWeight(10);
+    text("You WIN! ",width/2.5, height/3);
+    text("Click 3 Times to Restart ",width/4, height/2);
+    Win.play();
+    noLoop();
+   }
+
     if (bullet.y < 0) { 
       bullets.splice(i, 1);
     }
@@ -188,12 +259,20 @@ function mouseMove(){
     PlayerY = 401;
   }
 }
-function mouseClicked(){ //add a cooldown later
+function mouseClicked(){ //add a cooldown later maybe?
   let bullet = {
     x: PlayerX, 
     y: PlayerY,
     bulletSpeed: -8
   };
-  mySound.play();
-  bullets.push(bullet);
+  if (!GameOver){
+    PlayerShoot.play();
+    bullets.push(bullet);
+  } else {
+    restartDeath += 1;
+    
+  }
+  if (restartDeath > 2){
+    restartGame();
+  }
 }
